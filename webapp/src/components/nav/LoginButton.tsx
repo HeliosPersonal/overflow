9b1ec00d737cadf65b822815521b3c001f776fe7
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2026-03-22 00:49:03 +0100

    refactor: remove Link component from RegisterButton and LoginButton for cleaner code

--- webapp/src/components/nav/LoginButton.tsx
+++ webapp/src/components/nav/LoginButton.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import {Button} from "@heroui/button";
-import Link from "next/link";
 
 export default function LoginButton() {
     return (
         <Button
-            as={Link}
             href='/login?callbackUrl=/'
             color='primary'
             variant='bordered'
--- webapp/src/components/nav/RegisterButton.tsx
+++ webapp/src/components/nav/RegisterButton.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import {Button} from "@heroui/button";
-import Link from "next/link";
 
 export default function RegisterButton() {
     return (
         <Button
-            as={Link}
             href='/signup?callbackUrl=/'
             color='primary'
         >
